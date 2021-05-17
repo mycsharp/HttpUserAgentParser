fb2732d7ee880bf0f3872e1479e1e6c273cc878f
Benjamin Abt <mail@benjamin-abt.com>
2021-05-17 20:21:08 +0200

    add docs

--- src/MyCSharp.HttpUserAgentParser.AspNetCore/HttpUserAgentParserAccessor.cs
+++ src/MyCSharp.HttpUserAgentParser.AspNetCore/HttpUserAgentParserAccessor.cs
@@ -5,26 +5,48 @@ using MyCSharp.HttpUserAgentParser.Providers;
 
 namespace MyCSharp.HttpUserAgentParser.AspNetCore
 {
+    /// <summary>
+    /// User Agent parser accessor
+    /// </summary>
     public interface IHttpUserAgentParserAccessor
     {
+        /// <summary>
+        /// User agent value
+        /// </summary>
         string HttpContextUserAgent { get; }
+
+        /// <summary>
+        /// Returns current <see cref="HttpUserAgentInformation"/>
+        /// </summary>
         HttpUserAgentInformation Get();
     }
 
+    /// <summary>
+    /// User Agent parser accessor. Implements <see cref="IHttpContextAccessor.HttpContext"/>
+    /// </summary>
     public class HttpUserAgentParserAccessor : IHttpUserAgentParserAccessor
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly IHttpUserAgentParserProvider _httpUserAgentParser;
 
+        /// <summary>
+        /// Creates a new instance of <see cref="HttpUserAgentParserAccessor"/>
+        /// </summary>
         public HttpUserAgentParserAccessor(IHttpContextAccessor httpContextAccessor, IHttpUserAgentParserProvider httpUserAgentParser)
         {
             _httpContextAccessor = httpContextAccessor;
             _httpUserAgentParser = httpUserAgentParser;
         }
 
+        /// <summary>
+        /// User agent of current <see cref="IHttpContextAccessor"/>
+        /// </summary>
         public string HttpContextUserAgent =>
             _httpContextAccessor.HttpContext?.Request?.Headers["User-Agent"].ToString()!;
 
+        /// <summary>
+        /// Returns current <see cref="HttpUserAgentInformation"/> of current <see cref="IHttpContextAccessor"/>
+        /// </summary>
         public HttpUserAgentInformation Get()
             => _httpUserAgentParser.Parse(this.HttpContextUserAgent);
     }
