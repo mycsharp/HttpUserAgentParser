443987a621b497fac6dee94446d2376db85c2773
Günther Foidl <gue@korporal.at>
2021-05-17 11:18:48 +0200

    Added benchmark project for comparison with other libraries

--- perf/MyCSharp.HttpUserAgentParser.Benchmarks/Program.cs
+++ perf/MyCSharp.HttpUserAgentParser.Benchmarks/Program.cs
@@ -1,14 +1,6 @@
 // Copyright © myCSharp 2020-2021, all rights reserved
 
+using System.Reflection;
 using BenchmarkDotNet.Running;
 
-namespace MyCSharp.HttpUserAgentParser.Benchmarks
-{
-    class Program
-    {
-        static void Main()
-        {
-            BenchmarkRunner.Run<UserAgentBenchmarks>();
-        }
-    }
-}
+BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).Run(args);
