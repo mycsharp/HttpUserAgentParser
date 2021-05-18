44d7ed94e404ce5d7cfa648fb7103434499d86f6
Günther Foidl <gue@korporal.at>
2021-05-18 14:45:07 +0200

    Strong name signing of assemblies (#18)
    * Enabled .Net Standard 2.0 too
    
    * Make .NS 2.0 code cleaner
    
    * Strong name signing
    
    * Fixed CI
    
    * Make string.Contains on NS2.0 the same behavior as NS2.1

---
+++ src/NetStandard2.0-Shim.cs
@@ -0,0 +1,30 @@
+// Copyright © myCSharp 2020-2021, all rights reserved
+
+namespace System.Diagnostics.CodeAnalysis
+{
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    internal sealed class NotNullWhenAttribute : Attribute
+    {
+        public NotNullWhenAttribute(bool returnValue) => this.ReturnValue = returnValue;
+
+        public bool ReturnValue { get; }
+    }
+}
+
+namespace MyCSharp.HttpUserAgentParser
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal static class Extensions
+    {
+        public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> kvp, out TKey key, out TValue value)
+        {
+            key = kvp.Key;
+            value = kvp.Value;
+        }
+
+        public static bool Contains(this string instance, string value, StringComparison comparison)
+            => instance.IndexOf(value, comparison) >= 0;
+    }
+}
