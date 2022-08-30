1dae121a5afc5db708409de405aff367ad6729c4
BEN ABT <BenjaminAbt@users.noreply.github.com>
2022-08-30 19:34:48 +0200

    Update src/MyCSharp.HttpUserAgentParser/HttpContextExtensions.cs
    Co-authored-by: Günther Foidl <gue@korporal.at>

--- src/MyCSharp.HttpUserAgentParser/HttpContextExtensions.cs
+++ src/MyCSharp.HttpUserAgentParser/HttpContextExtensions.cs
@@ -12,6 +12,13 @@ public static class HttpContextExtensions
     /// <summary>
     /// Returns the User-Agent header value
     /// </summary>
-    public static string? GetUserAgentString(this HttpContext httpContext) =>
-         httpContext.Request?.Headers["User-Agent"].ToString()!;
+    public static string? GetUserAgentString(this HttpContext httpContext)
+    {
+        if (httpContext.Request.Headers.TryGetValue("User-Agent", out StringValues value))
+        {
+            return value;
+        }
+
+        return null;
+    }
 }
